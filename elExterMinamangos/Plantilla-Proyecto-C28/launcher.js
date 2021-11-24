609847d6efbf50fc0c678ec36db5ce2595816e21
AgenteC <90355023+AgenteC@users.noreply.github.com>
2021-11-24 17:03:49 -0600

    Add files via upload

---
+++ elExterMinamangos/Plantilla-Proyecto-C28/launcher.js
@@ -0,0 +1,14 @@
+class launcher
+
+function attach(){
+
+    this.bodyA = this.bodyA
+    this.bodyB = this.bodyB
+    attach(body){ this.launcher.bodyA=body};
+}
+fly()
+{
+
+    this.launcher.bodyA=nulls
+}
+
